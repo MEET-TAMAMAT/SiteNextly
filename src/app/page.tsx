e2fa8deec0fdf43bfcced21c4e13d8117e109c66
Andri <meet.tamamat@gmail.com>
2026-04-14 16:21:16 +0300

    Modified Hero.tsx and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,46 +12,12 @@ async function fetchSingle(collection: string) {
   }
 }
 
-async function fetchMany(collection: string) {
-  try {
-    const data = await directus.request(readItems(collection));
-    return data || [];
-  } catch (error) {
-    console.error(`DIRECTUS ERROR (${collection}):`, error);
-    return [];
-  }
-}
-
 export default async function Home() {
-  const [
-    heroData,
-    howItWorksData,
-    howItWorksStepsData,
-    featuresData,
-    featuresItemsData,
-    pricingPlansData,
-    faqItemsData,
-    testimonialsData,
-    contactData,
-    contactFormData,
-  ] = await Promise.all([
-    fetchSingle("hero"),
-    fetchSingle("how_it_works"),
-    fetchMany("how_it_works_steps"),
-    fetchSingle("features"),
-    fetchMany("features_items"),
-    fetchMany("pricing_plans"),
-    fetchMany("faq_items"),
-    fetchMany("testimonials"),
-    fetchSingle("contact"),
-    fetchSingle("contact_form"),
-  ]);
+  const heroData = await fetchSingle("hero");
 
   return (
     <Container>
       <Hero data={heroData} />
-
-      {/* Next sections will be connected one by one */}
     </Container>
   );
 }
